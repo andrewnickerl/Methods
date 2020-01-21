036263e6e6e472b67f8c47f96b6e0818b4e4d7a6
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-01-21 18:55:12 -0500

    Merged division and remainder functions to create a function to calculate division and also display the remainder

--- Methods/MainPage.xaml.cs
+++ Methods/MainPage.xaml.cs
@@ -53,13 +53,9 @@ namespace Methods
                 }
                 else if (division.IsChecked.HasValue && division.IsChecked.Value)
                 {
-                    calculatedValue = divideValues(leftHandSide, rightHandSide);
-                    showResult(calculatedValue);
-                }
-                else if (remainder.IsChecked.HasValue && remainder.IsChecked.Value)
-                {
-                    calculatedValue = remainderValues(leftHandSide, rightHandSide);
-                    showResult(calculatedValue);
+                    int division, remainder;
+                    (division, remainder) = divide(leftHandSide, rightHandSide);
+                    result.Text = $"{division} remainder {remainder}";
                 }
             }
             catch (Exception caught)
@@ -87,16 +83,12 @@ namespace Methods
             return leftHandSide * rightHandSide;
         }
 
-        private int divideValues(int leftHandSide, int rightHandSide)
+        private (int, int) divide(int leftHandSide, int rightHandSide)
         {
             expression.Text = $"{leftHandSide} / {rightHandSide}";
-            return leftHandSide / rightHandSide;
-        }
-
-        private int remainderValues(int leftHandSide, int rightHandSide)
-        {
-            expression.Text = $"{leftHandSide} % {rightHandSide}";
-            return leftHandSide % rightHandSide;
+            int division = leftHandSide / rightHandSide;
+            int remainder = leftHandSide % rightHandSide;
+            return (division, remainder);
         }
 
         private void showResult(int answer) => result.Text = answer.ToString();
